f5e54aa76964ae0c55dafcb16c1cfb2654fd8b7c
Samuel Paiva Bernardes <samuelpbernardes@yahoo.com.br>
2021-12-23 19:26:51 -0300

    Quick Sort - Commit Quarto

--- quicksort.cpp
+++ quicksort.cpp
@@ -14,7 +14,7 @@ void troca(int *e1, int *e2)
     *e2 = t;
 }
 
-int particionamentoQuickSort(int vetor[], int a, int b)
+int particionamentoQuickSort(int vetor[], int a, int b, int *contadorTroca, int *contadorComparacao)
 {
     int pivo = vetor[b]; // pivô
     int i = (a - 1);     // Índice do elemento menor
@@ -24,25 +24,46 @@ int particionamentoQuickSort(int vetor[], int a, int b)
         //Se o elemento atual é menor ou igual ao pivô
         if (vetor[j] <= pivo)
         {
+            (*contadorComparacao)++; //Conta o numero de comparacoes feitas
+
             i++; // aumenta o índice do elemento menor
             troca(&vetor[i], &vetor[j]);
+
+            (*contadorTroca)++; // Conta o numero de trocas feitas 
         }
     }
     troca(&vetor[i + 1], &vetor[b]);
+    (*contadorTroca)++; //Conta o numero de trocas feitas
+
     return (i + 1);
 }
 
 void ordenacaoQuickSort(int vetor[], int a, int b)
 {
+    int comparacao;
+    int qnt_troca;
+    int tempo;
+    
+    comparacao = 0;
+    qnt_troca = 0; 
+    tempo = 0;
+
     if (a < b)
     {
 
-        int indiceParticionamento = particionamentoQuickSort(vetor, a, b);
+        int indiceParticionamento = particionamentoQuickSort(vetor, a, b, &qnt_troca, &comparacao);
 
         // Faz o sort dos elementos em separado, de cada partição
         ordenacaoQuickSort(vetor, a, indiceParticionamento - 1);
         ordenacaoQuickSort(vetor, indiceParticionamento + 1, b);
+
+
     }
+
+    cout << "Quantidade de troca " << qnt_troca;
+    cout << "Comparacao" << comparacao;
+    cout << "Tempo em execucao " << tempo;
+
 }
 
 
