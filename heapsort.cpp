2b890c859015e3b100212130e6f0c9ec3a4f17cf
Denner Efisio <dennerefisio@gmail.com>
2021-12-23 19:43:22 -0300

    adicionando metricas no heapsort

--- heapsort.cpp
+++ heapsort.cpp
@@ -2,38 +2,51 @@
 
 using namespace std;
 
-void heapficar(int vetor[], int n, int i)
+void heapficar(int vetor[], int n, int i, int *comparacoes, int *trocas)
 {
-    int esquerda = 2*i +1;
-    int direita = 2*i +2;
+    int esquerda = 2 * i + 1;
+    int direita = 2 * i + 2;
     int maior = i;
 
-    if(esquerda < n && vetor[esquerda] > vetor[i]){
+    if (esquerda < n && vetor[esquerda] > vetor[i])
+    {
         maior = esquerda;
+        comparacoes++;
     }
-    if (direita < n && vetor[direita] > vetor[maior]){
+    if (direita < n && vetor[direita] > vetor[maior])
+    {
         maior = direita;
+        comparacoes++;
     }
-    if(maior != i){
+    if (maior != i)
+    {
         swap(vetor[i], vetor[maior]);
-        heapficar(vetor, n, maior);
+        trocas++;
+        heapficar(vetor, n, maior, comparacoes, trocas);
     }
 }
 
-void construirHeap(int vetor[], int tamanhoVetor)
+void construirHeap(int vetor[], int tamanhoVetor, int *comparacoes, int *trocas)
 {
     for (int i = (tamanhoVetor / 2) - 1; i >= 0; i--)
     {
-        heapficar(vetor, tamanhoVetor, i);
+        heapficar(vetor, tamanhoVetor, i, comparacoes, trocas);
     }
 }
 
 void heapsort(int vetor[], int tamanhoVetor)
 {
-    construirHeap(vetor, tamanhoVetor);
+    int *comparacoes = 0, *trocas = 0;
+    clock_t start, end;
+    start = clock();
+
+    construirHeap(vetor, tamanhoVetor, comparacoes, trocas);
     for (int i = tamanhoVetor - 1; i >= 0; i--)
     {
         swap(vetor[0], vetor[1]);
-        heapficar(vetor, i, 0);
+        trocas++;
+        heapficar(vetor, i, 0, comparacoes, trocas);
     }
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
 }
